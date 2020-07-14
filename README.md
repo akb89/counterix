6cf16b28335b7daa541d81d03ff64098242cb590
AKB <akb@3azouz.net>
2020-07-14 12:58:20 +0200

    Added github workflow

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # counterix
-Count-based Distributional Semantic Model
+Generarating count-based PPMI-weighed SVD DSM.
+
+## Install
+
+## Use
+
+### Generate
+To generate a raw count matrix from a tokenized corpus, run:
+```
+counterix generate \
+  --corpus /abs/path/to/corpus/txt/file \
+  --min-count frequency_threshold \
+  --win-size window_size \
+  --output abs/path/to/output/model/directory
+```
+
+If the `--output` parameter is not set, the output files will be saved to the corpus directory.
+
+### Weigh
+To weigh a raw count model with PPMI, run:
+```
+counterix weigh \
+  --model /abs/path/to/raw/count/npz/model \
+  --output /abs/path/to/output/model/directory \
+  --weighing-func ppmi
+```
+
+### SVD
+To apply SVD on a PPMI-weighed model, with k=10000, run:
+```
+counterix svd \
+  --model /abs/path/to/ppmi/npz/model \
+  --dim 10000 \
+  --which LM  # largest singular values
+```
+
+## Warning
+For informativeness, np.dot in `prob_values = np.exp(np.dot(l1, self._model.trainables.syn1neg.T))` using multithreading by default with openblas. Need to run the code with `env OMP_NUM_THREADS=1 counterix generate...`
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""counterix setup.py.
+
+This file details modalities for packaging the counterix application.
+"""
+
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='entropix',
+    description='Generating count-based Distributional Semantic Modelss',
+    author=' Alexandre Kabbach',
+    author_email='akb@3azouz.net',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.1.0',
+    url='https://github.com/akb89/counterix',
+    download_url='https://github.com/akb89/counterix',
+    license='MIT',
+    keywords=['count-based', 'distributional semantics'],
+    platforms=['any'],
+    packages=['counterix', 'counterix.logging', 'counterix.exceptions',
+              'counterix.utils', 'counterix.core', 'counterix.resources',
+              'counterix.filtering'],
+    package_data={'counterix': ['logging/*.yml', 'resources/*']},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'counterix = counterix.main:main'
+        ],
+    },
+    install_requires=['pyyaml>=4.2b1'],
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'Environment :: Web Environment',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Education',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: MIT License',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Topic :: Scientific/Engineering :: Artificial Intelligence',
+                 'Topic :: Software Development :: Libraries :: Python Modules'],
+    zip_safe=False,
+)
